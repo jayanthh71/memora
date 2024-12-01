b11bf96fe5250a22abb97c3e53b9a845de06c82d
jayanthh71 <jayanth37069@gmail.com>
2024-12-01 12:46:05 +0530

    Get user details

--- src/app/utils/dauth.ts
+++ src/app/utils/dauth.ts
@@ -2,6 +2,6 @@ import { redirect } from "next/navigation";
 
 export default async function DAuth() {
   redirect(
-    `https://auth.delta.nitt.edu/authorize?client_id=${process.env.DAUTH_ID}&redirect_uri=localhost:3000/api/auth/dauth`,
+    `https://auth.delta.nitt.edu/authorize?client_id=${process.env.DAUTH_ID}&redirect_uri=${process.env.DAUTH_REDIRECT}&scope=user`,
   );
 }
