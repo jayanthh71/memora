bd1d469ca75f39121b4a4a71e24e3c317a753c10
jayanthh71 <jayanth37069@gmail.com>
2025-01-07 13:29:41 +0400

    Initialize mongodb integration

--- src/auth.ts
+++ src/auth.ts
@@ -1,6 +1,10 @@
+import { connectDB } from "@/lib/database";
+import { MongoDBAdapter } from "@auth/mongodb-adapter";
 import NextAuth from "next-auth";
 import Google from "next-auth/providers/google";
 
 export const { handlers, signIn, signOut, auth } = NextAuth({
   providers: [Google],
+  adapter: MongoDBAdapter(connectDB()),
+  callbacks: {},
 });
