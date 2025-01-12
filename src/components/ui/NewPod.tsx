401fe2e8df3963a0fd87cf89c33d1a935335ba6d
jayanthh71 <jayanth37069@gmail.com>
2025-01-12 23:11:00 +0530

    Finish NewPod

--- src/components/ui/NewPod.tsx
+++ src/components/ui/NewPod.tsx
@@ -2,11 +2,27 @@ import Link from "next/link";
 
 export default function NewPod() {
   return (
-    <Link
-      className="flex h-80 w-72 min-w-min flex-col items-center justify-center rounded-xl border-2 border-accent bg-secondary p-4 text-center text-text transition-shadow hover:shadow-[0_0_25px_5px_rgba(0,0,0,0.2)] hover:shadow-accent"
-      href="/pods/new"
-    >
+    <div className="flex h-80 w-72 min-w-min flex-col items-center justify-center space-y-5 rounded-xl border-2 border-accent bg-secondary p-4 text-center text-text transition-shadow hover:shadow-[0_0_25px_5px_rgba(0,0,0,0.2)] hover:shadow-accent">
       <h1 className="font-heading text-xl">Create New Pod</h1>
-    </Link>
+      <Link
+        className="flex h-20 w-20 items-center justify-center rounded-full bg-accent text-text"
+        href="/pods/new"
+      >
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          className="h-12 w-12"
+          fill="none"
+          viewBox="0 0 24 24"
+          stroke="currentColor"
+        >
+          <path
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            strokeWidth={1.75}
+            d="M12 6v6m0 0v6m0-6h6m-6 0H6"
+          />
+        </svg>
+      </Link>
+    </div>
   );
 }
--- src/components/ui/Pod.tsx
+++ src/components/ui/Pod.tsx
@@ -11,7 +11,7 @@ const pod = {
 
 export default function Pod() {
   return (
-    <div className="flex h-80 w-72 min-w-min cursor-pointer flex-col items-center justify-between rounded-xl border-2 border-accent bg-secondary p-4 text-center text-text transition-shadow hover:shadow-[0_0_25px_5px_rgba(0,0,0,0.2)] hover:shadow-accent">
+    <div className="flex h-80 w-72 min-w-min flex-col items-center justify-between rounded-xl border-2 border-accent bg-secondary p-4 text-center text-text transition-shadow hover:shadow-[0_0_25px_5px_rgba(0,0,0,0.2)] hover:shadow-accent">
       <h1 className="line-clamp-2 font-heading text-xl">{pod.name}</h1>
       <p className="line-clamp-5 font-body text-lg">{pod.description}</p>
       <p className="font-body text-lg">Due Date: {pod.date_due}</p>
